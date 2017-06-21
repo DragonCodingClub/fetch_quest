0c1c713c08a4b93096f4d503c68220a038d8ac84
BijanBM <bijanboy@gmail.com>
2017-06-21 19:15:33 -0400

    Update window.h

--- fetch_quest/window.h
+++ fetch_quest/window.h
@@ -2,14 +2,14 @@
 #define WINDOW_H
 #include"includes.h"
 struct Window{
-    SDL_Window *win;
-    SDL_Renderer *ren;
-    SDL_DisplayMode desktop;
-    bool fullscreen;
-    Uint16 screenWidth,screenHeight,xOffset,yOffset;
-    Window(char*);
-    void setAspect();
-    void toggleFullscreen();
-    void setSize(SDL_Event e);
+	    SDL_Window *win;
+	    SDL_Renderer *ren;
+	    SDL_DisplayMode desktop;
+	    bool fullscreen;
+	    Uint16 screenWidth,screenHeight,xOffset,yOffset;
+	    Window(char*);
+	    void setAspect();
+	    void toggleFullscreen();
+	    void setSize(SDL_Event e);
 };
 #endif // WINDOW_H_INCLUDED
