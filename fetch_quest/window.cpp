d37aa645ebb149c469b6c9d99cea66ead0cbe690
BijanBM <bijanboy@gmail.com>
2017-06-21 19:13:24 -0400

    Update window.cpp

--- fetch_quest/window.cpp
+++ fetch_quest/window.cpp
@@ -2,15 +2,15 @@
 #include"includes.h"
 const double aspectRatio=1.777777778;
 Window::Window(char *name){
-    screenWidth=640;
-    screenHeight=360;
+	screenWidth=640;
+	screenHeight=360;
 	SDL_GetDesktopDisplayMode(0,&desktop);
 	win=SDL_CreateWindow(name,SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,640,360,SDL_WINDOW_SHOWN|SDL_WINDOW_RESIZABLE),
-    ren=SDL_CreateRenderer(win,-1,SDL_RENDERER_SOFTWARE);
-    fullscreen=false;
+	ren=SDL_CreateRenderer(win,-1,SDL_RENDERER_SOFTWARE);
+	fullscreen=false;
 }
 void Window::setAspect(){
-    if(screenHeight*aspectRatio<screenWidth){
+	if(screenHeight*aspectRatio<screenWidth){
 		yOffset=0;
 		xOffset=(screenWidth-screenHeight*aspectRatio)/2;
 		screenWidth=screenHeight*aspectRatio;
@@ -22,15 +22,15 @@ void Window::setAspect(){
 	}
 }
 void Window::toggleFullscreen(){
-    fullscreen=!fullscreen;
-    screenWidth=fullscreen?desktop.w:640;
-    screenHeight=fullscreen?desktop.h:360;
-    SDL_SetWindowSize(win,screenWidth,screenHeight);
-    setAspect();
-    SDL_SetWindowFullscreen(win,fullscreen?SDL_TRUE:SDL_FALSE);
+	    fullscreen=!fullscreen;
+	    screenWidth=fullscreen?desktop.w:640;
+	    screenHeight=fullscreen?desktop.h:360;
+	    SDL_SetWindowSize(win,screenWidth,screenHeight);
+	    setAspect();
+	    SDL_SetWindowFullscreen(win,fullscreen?SDL_TRUE:SDL_FALSE);
 }
 void Window::setSize(SDL_Event e){
-    screenWidth=e.window.data1;
-    screenHeight=e.window.data2;
-    setAspect();
+	    screenWidth=e.window.data1;
+	    screenHeight=e.window.data2;
+	    setAspect();
 }
